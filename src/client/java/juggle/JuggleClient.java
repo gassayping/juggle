537f76e49d89b60a8feb3b0bae83626816f206e2
Gassayping <Dev_Gassayping@proton.me>
2024-06-06 17:19:32 -0700

    Initial Commit

---
+++ src/client/java/juggle/JuggleClient.java
@@ -0,0 +1,10 @@
+package juggle;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JuggleClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
