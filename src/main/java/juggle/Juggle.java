07d9b0eab9640dffa3e361ef7a529556408faced
Gassayping <Dev_Gassayping@proton.me>
2024-06-06 19:16:54 -0700

    Added juggler's ball item
    No functionality, just an item with a use method

--- src/main/java/juggle/Juggle.java
+++ src/main/java/juggle/Juggle.java
@@ -1,22 +1,24 @@
 package juggle;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item.Settings;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Juggle implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
 	public static final Logger LOGGER = LoggerFactory.getLogger("juggle");
+
+	public static final JugglersBall JUGGLERS_BALL = new JugglersBall(new Settings());
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.ITEM, Identifier.of("juggle", "jugglers_ball"), JUGGLERS_BALL);
+		LOGGER.info("Juggle mod loaded");
 	}
 }
---
+++ src/main/java/juggle/JugglersBall.java
@@ -0,0 +1,27 @@
+package juggle;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class JugglersBall extends Item {
+
+	public JugglersBall(Settings settings) {
+		super(settings);
+	}
+
+	@Override
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+		user.playSound(SoundEvents.BLOCK_ANVIL_PLACE, 1.0f, 1.0f);
+
+		ItemStack item = user.getStackInHand(hand);
+		// item.setCount(0);
+
+		return TypedActionResult.success(item);
+	}
+
+}
