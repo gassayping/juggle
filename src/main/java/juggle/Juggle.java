6fffc9a29b8bcd4a7fa8eb76f16bd4cfe1ee8ddb
Gassayping <Dev_Gassayping@proton.me>
2024-06-06 22:57:18 -0700

    Added texture and lang key for juggler's ball
    Texture is likely to change

--- src/main/java/juggle/Juggle.java
+++ src/main/java/juggle/Juggle.java
@@ -1,6 +1,7 @@
 package juggle;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
 import net.minecraft.item.Item.Settings;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -11,14 +12,18 @@ import org.slf4j.LoggerFactory;
 
 public class Juggle implements ModInitializer {
 
-	public static final Logger LOGGER = LoggerFactory.getLogger("juggle");
+	public static final String MOD_ID = "juggle";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	public static final JugglersBall JUGGLERS_BALL = new JugglersBall(new Settings());
 
 	@Override
 	public void onInitialize() {
-
-		Registry.register(Registries.ITEM, Identifier.of("juggle", "jugglers_ball"), JUGGLERS_BALL);
+		register(JUGGLERS_BALL, "jugglers_ball");
 		LOGGER.info("Juggle mod loaded");
+	}
+
+	private static <T extends Item> T register(T item, String name) {
+		return Registry.register(Registries.ITEM, Identifier.of("juggle", name), item);
 	}
 }
